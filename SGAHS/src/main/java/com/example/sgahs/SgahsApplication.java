5ccc7376b101a7ffea5b6376d6c673abce4c8727
Juan Pastelin <pastelin406@gmail.com>
2022-05-12 21:33:42 -0500

    Commit inicial

---
+++ SGAHS/src/main/java/com/example/sgahs/SgahsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sgahs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgahsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgahsApplication.class, args);
+	}
+
+}
