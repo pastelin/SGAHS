7f2c3861dd2463003939dd3849c745929c4fdb0b
Juan Pastelin <pastelin406@gmail.com>
2023-02-22 20:43:48 -0600

    Refactoriza gastos

--- SGAHS/src/main/java/com/sgahs/springboot/app/models/dao/IAhorroDao.java
+++
@@ -1,9 +0,0 @@
-package com.sgahs.springboot.app.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sgahs.springboot.app.models.entity.Ahorro;
-
-public interface IAhorroDao extends CrudRepository<Ahorro, Long>{
-
-}
--- SGAHS/src/main/java/com/sgahs/springboot/app/models/dao/IGastoRecurrenteDao.java
+++
@@ -1,9 +0,0 @@
-package com.sgahs.springboot.app.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sgahs.springboot.app.models.entity.GastoRecurrente;
-
-public interface IGastoRecurrenteDao extends CrudRepository<GastoRecurrente, Integer> {
-
-}
